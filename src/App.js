ecdf722ff1a27111acb3b9fa5bce415c1bb05b8b
John Christian P. Oraa <51404378+johnchristianoraa@users.noreply.github.com>
2020-05-25 14:22:47 +0800

    first component created

--- src/App.js
+++ src/App.js
@@ -1,1 +1,57 @@
 import React from 'react'
+
+const Avatar = (props) => {
+    return ( 
+        <img className="Avatar"
+            src={props.user.avatarUrl}
+            alt={props.user.name}
+        />
+    );
+}
+const UserInfo = (props) =>{
+    return (
+        <div className="UserInfo">
+            <Avatar user= {props.user} />
+            <div className="UserInfo-name">
+                {props.user.name}
+            </div>
+        </div>
+    );
+}
+
+const formatDate = (date) =>{
+    return date.toLocaleDateString();
+}
+
+const Comment = (props) => {
+    return (
+        <div className="Comment">
+            <UserInfo user = {props.author}/>
+            <div className="Comment-text">{props.text}</div>
+            <div className="Comment-date">{formatDate(props.date)}</div>
+        </div>
+    );
+}
+
+const comment = {
+    date: new Date(),
+    text: 'I hope you enjoy learning React!',
+    author: {
+      name: 'Hello Kitty',
+      avatarUrl: 'https://placekitten.com/g/64/64',
+    },
+};
+
+const App = () =>{
+    return (
+        <div>
+            <Comment 
+                author={comment.author}
+                text={comment.text}
+                date={comment.date}
+            />
+        </div>
+    );
+}
+
+export default App
--- src/index.js
+++ src/index.js
@@ -2,9 +2,4 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
-// ReactDOM.render(<App/>, document.getElementById('root'));
-const Welcome = (props) =>{
-    return <h1>Hello, {props.name}</h1>
-} 
-
-ReactDOM.render(<Welcome name="John"/>, document.getElementById('root'));
+ReactDOM.render(<App/>, document.getElementById('root'));
